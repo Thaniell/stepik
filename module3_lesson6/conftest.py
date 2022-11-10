6723e8b3af375eb99dcaad7c2a24c4b80abc3e96
Thaniell <thaniellfox@gmail.com>
2022-11-10 18:21:42 +0300

    update conftest.py, add step2 and step3

--- module3_lesson6/conftest.py
+++ module3_lesson6/conftest.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 @pytest.fixture(scope="function")
 def browser():
     print("\nstart browser for test..")
-    browser = webdriver.Chrome()
+    browser = webdriver.Chrome(
     yield browser
     print("\nquit browser..")
     browser.quit()
